e8a129100bbbab06a7929932d1547b37e2217778
heeay <hajumosa1313@gmail.com>
2023-11-13 05:54:44 +0900

    정리

--- pages/log-in.tsx
+++ pages/log-in.tsx
@@ -21,13 +21,9 @@ const Enter: NextPage = () => {
   useMutation<MutationResult>("/api/users/enter");
   const { register, handleSubmit, reset } = useForm<EnterForm>();
 
-  const [method, setMethod] = useState<"email" | "phone">("email");
 
 
-  const onEmailClick = () => {
-    reset();
-    setMethod("userId");
-  };
+
   const onValid = (validForm: EnterForm) => {
     if (loading) return;
     enter(validForm);
@@ -37,94 +33,34 @@ const Enter: NextPage = () => {
 
   return (
 <div className="mt-16 px-4">
-  <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
+  <h3 className="text-3xl font-bold text-center">Tweeter</h3>
     <div className="mt-12">
-    {data?.ok ? (
           <form
-            onSubmit={tokenHandleSubmit(onTokenValid)}
+            onSubmit={handleSubmit(onValid)}
             className="flex flex-col mt-8 space-y-4"
           >
-            <Input
-              register={tokenRegister("token", {
-                required: true,
-              })}
-              name="token"
-              label="Confirmation Token"
-              type="number"
-              required
-            />
-            
-          </form>
-        ) : (
-          <>
-            <div className="flex flex-col items-center">
-              <h5 className="text-sm text-gray-500 font-medium">
-                Enter using:
-              </h5>
-              <div className="grid border-b  w-full mt-8 grid-cols-2 ">
-                <button
-                  className={cls(
-                    "pb-4 font-medium text-sm border-b-2",
-                    method === "email"
-                      ? " border-orange-500 text-orange-400"
-                      : "border-transparent hover:text-gray-400 text-gray-500"
-                  )}
-                  onClick={onEmailClick}
-                >
-                  Email
-                </button>
-                <button
-                  className={cls(
-                    "pb-4 font-medium text-sm border-b-2",
-                    method === "phone"
-                      ? " border-orange-500 text-orange-400"
-                      : "border-transparent hover:text-gray-400 text-gray-500"
-                  )}
-                  onClick={onPhoneClick}
-                >
-                  Phone
-                </button>
-              </div>
-            </div>
-            <form
-              onSubmit={handleSubmit(onValid)}
-              className="flex flex-col mt-8 space-y-4"
-            >
-              {method === "email" ? (
-                <Input
-                  register={register("email", {
-                    required: true,
-                  })}
-                  name="email"
-                  label="Email address"
-                  type="email"
-                  required
-                />
-              ) : null}
-              {method === "phone" ? (
-                <Input
-                  register={register("phone")}
-                  name="phone"
-                  label="Phone number"
-                  type="number"
-                  kind="phone"
-                  required
-                />
-              ) : null}
-              {method === "email" ? (
-                <Button text={loading ? "Loading" : "Get login link"} />
-              ) : null}
-              {method === "phone" ? (
-                <Button text={loading ? "Loading" : "Get one-time password"} />
-              ) : null}
-            </form>
-          </>
-        )}
+              <Input
+                register={register("userId", {
+                  required: true,
+                })}
+                name="userId"
+                label="Email address"
+                type="email"
+                required
+              />
+              <Input
+                register={register("userName", {
+                  required: true,
+                })}
+                name="userName"
+                label="userName"
+                type="text"
+                required
+              />
+              <button>Login</button>
+          </form> 
     </div>
 </div>
   );
-
-
-
-
 }
+export default Enter;
